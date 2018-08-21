4d79fa73e525704968670eda9d82e4f890a3347f
Arianna <heartofthefox87@gmail.com>
2018-08-21 14:29:37 -0500

    Lect 31 complete

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+
+  // Selectors can be elements, class, or css selector
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
+
+  // Must have template or remplateUrl.
   // template: `
   //   <app-server></app-server>
   //   <app-server></app-server>`,
@@ -12,14 +16,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'no server was created';
+  serverName = 'TestServer';
 
   constructor() {
+    // Enables creation of server after 2 seconds
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
